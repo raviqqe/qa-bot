31da574985dae156d64ea6fd06d1c413747704b7
Yota Toyama <raviqqe@gmail.com>
2019-01-01 10:08:49 +0000

    Use FIFO queue for scraping

--- dataset/dataset/settings.py
+++ dataset/dataset/settings.py
@@ -3,3 +3,6 @@ SPIDER_MODULES = ["dataset.spiders"]
 NEWSPIDER_MODULE = "dataset.spiders"
 ROBOTSTXT_OBEY = True
 FEED_EXPORT_ENCODING = "utf-8"
+DEPTH_PRIORITY = 1
+SCHEDULER_DISK_QUEUE = "scrapy.squeue.PickleFifoDiskQueue"
+SCHEDULER_MEMORY_QUEUE = "scrapy.squeue.FifoMemoryQueue"
