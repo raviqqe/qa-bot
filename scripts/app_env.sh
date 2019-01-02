ea277e1e3ae331ecca756776bb562f5541843a53
Yota Toyama <raviqqe@gmail.com>
2019-01-02 09:14:42 +0000

    Deploy on Cloud Build

--- scripts/app_env.sh
+++ scripts/app_env.sh
@@ -6,7 +6,3 @@ for var in CONSUMER_KEY CONSUMER_SECRET ACCESS_TOKEN_KEY ACCESS_TOKEN_SECRET
 do
 	echo "  $var: $(eval "echo \$$var")" >> app.yaml
 done
-
-gcloud app deploy --version production
-
-git checkout app.yaml
